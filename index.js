7eaa345986545c5d00b4068fd7da9ff071ee056c
Andrew Yates <yatesab@gmail.com>
2018-10-13 17:08:38 -0600

    setting up basic html page

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 var express = require('express');
+
+//App Setup
+var app = express();
+var server = app.listen(4000, function(){
+	console.log("listening on port 4000");
+});
+
+app.use(express.static('public'));
